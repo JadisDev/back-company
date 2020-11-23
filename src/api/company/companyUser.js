a110e46cb3bd62c51bd8ae2b4a342928ba19ff06
Jadis Jale <jadisjale@hotmail.com>
2020-11-23 02:05:46 -0300

    refatoracao

--- src/api/company/companyUser.js
+++ src/api/company/companyUser.js
@@ -5,7 +5,7 @@ const companySchema = new mongoose.Schema({
     name: {type: String, min:3, required: true},
     cnpj: {type: String, required: true},
     lat: {type: Number, required: true},
-    lng: {type: String, required: true},
+    lng: {type: Number, required: true},
     createdAt: {type: Date, default: Date.now},
     user: mongoose.Schema.Types.ObjectId
 })
